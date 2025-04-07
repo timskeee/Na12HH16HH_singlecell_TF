64fe021270c1c9c0f59184b1a38f98f72e83aca9
mleitner8 <127556229+mleitner8@users.noreply.github.com>
2025-04-07 19:36:24 -0400

    Add files via upload

---
+++ src/batch.py
@@ -0,0 +1,22 @@
+from netpyne.batchtools.search import search
+import numpy
+
+
+# Create parameter grid for search
+params = {'IClamp1.amp': [0.3, 0.4, 0.5, 0.6]}
+
+# use batch_sge_config if running on a
+shell_config = {'command': 'python init_batch.py',}
+
+search(job_type = 'sh',
+       comm_type       = "socket",
+       params          = params,
+       run_config      = shell_config,
+       label           = "grid_search",
+       output_path     = "./grid_batch",
+       checkpoint_path = "./ray",
+       num_samples     = 1,
+       metric          = 'epsp',
+       mode            = 'min',
+       algorithm       = "variant_generator",
+       max_concurrent  = 9)
