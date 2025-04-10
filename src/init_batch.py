dfc50466d8479194b1ae4139a08aa29987f5643d
mleitner8 <127556229+mleitner8@users.noreply.github.com>
2025-04-10 14:49:32 -0400

    Update init_batch.py

--- src/init_batch.py
+++ src/init_batch.py
@@ -6,5 +6,4 @@ from netParams import netParams
 cfg = specs.SimConfig()  # create a SimConfig object, can be provided with a dictionary on initial call to set initial values
 netParams = specs.NetParams()  # create a netParams object
 
-sim.createSimulate(netParams=netParams, simConfig=cfg)
 sim.createSimulateAnalyze(netParams, cfg)
