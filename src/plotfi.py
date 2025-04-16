c3e01ac84218d4a4221ba3aeba19102077d37198
mleitner8 <molly.leitner@downstate.edu>
2025-04-16 12:35:52 -0400

    updated plot FI curve

--- src/plotfi.py
+++ src/plotfi.py
@@ -4,18 +4,18 @@ from netpyne import sim
 
 from batch import params
 
-firing_rates = []  # List to store firing rates for each current value
+
 
 # Analyze spike data and compute firing rate
 spike_times = sim.analysis.popAvgRates() # Get spike times
 value_list = spike_times['PT5B']
 # Store firing rate for current
-firing_rates.append(value_list)
+
 
 
 #  Plot the F-I curve
 plt.figure(figsize=(8, 6))
-plt.plot(params, firing_rates, marker='o', linestyle='-', color='b')
+plt.plot(params, value_list, marker='o', linestyle='-', color='b')
 plt.xlabel('Injected Current (nA)')
 plt.ylabel('Firing Rate (Hz)')
 plt.title('F-I Curve')
