42bb8d085c2f0b75a7aa016faf562d792fee0789
mleitner8 <molly.leitner@downstate.edu>
2025-06-04 13:58:35 -0400

    changes to properly record vsoma

--- src/cfg.py
+++ src/cfg.py
@@ -58,7 +58,7 @@ cfg.weightNormThreshold = 4.0  # weight normalization factor threshold
 #------------------------------------------------------------------------------
 # Current Inputs
 #------------------------------------------------------------------------------
-cfg.addIClamp = 1 # change to 1 to add IClamps (can add multiple)
+cfg.addIClamp = 0 # change to 1 to add IClamps (can add multiple)
 
 cfg.IClamp1 = {'pop': 'PT5B' ,'sec': 'soma', 'loc': 0.5, 'start': 0, 'dur': 500, 'amp': 0.4}
 
@@ -66,9 +66,9 @@ cfg.IClamp1 = {'pop': 'PT5B' ,'sec': 'soma', 'loc': 0.5, 'start': 0, 'dur': 500,
 #------------------------------------------------------------------------------
 # NetStim Inputs
 #------------------------------------------------------------------------------
-cfg.addNetStim = 0   # change to 1 to add NetStims (can add multiple)
+cfg.addNetStim = 1   # change to 1 to add NetStims (can add multiple)
 
-cfg.NetStim1 = {'pop': 'PT5B', 'sec': 'dend_20', 'loc': 0.5, 'synMech': ['AMPA', 'NMDA'], 'synMechWeightFactor': [0.5, 0.5],
+cfg.NetStim1 = {'pop': 'PT5B', 'sec': 'soma', 'loc': 0.5, 'synMech': ['AMPA', 'NMDA'], 'synMechWeightFactor': [0.5, 0.5],
 				'start': 0, 'interval': 1000.0/40.0, 'noise': 0.0, 'number': 1000.0, 'weight': 0.5, 'delay': 0}
 
 #------------------------------------------------------------------------------
